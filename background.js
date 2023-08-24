aa7443735662ccd36cbec67484cd1c64a2b9adb0
Moritz Brandstätter <moritz.brandstaetter@twinformatics.at>
2023-08-24 14:27:19 +0200

    fix execution

--- background.js
+++ background.js
@@ -44,32 +44,79 @@ async function performTest() {
         return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
     }
 
-    function parseTestCase() {
-        chrome.storage.sync.get(
-            "testcase",
-            (items) => {
-                const testcase = items.testcase;
-
-                byLine(testcase).forEach(line => {
-                    const tokens = bySpaces(line);
-        
-                    switch (tokens[0]) {
-                        case 'click':
-                            const element = tokens[1].split(':');
-                            const expr = '//' + element[0] + '[@' + element[1] + '="' + tokens[2] + '"]';
-                            console.log('Click on', expr);
-                            break;
-                    }
-                });
-            }
+    async function parseTestCase() {
+        return chrome.storage.sync.get(
+            "testcase"
         );
     }
 
+    function getExpressionWithElementIdentifier(tokens) {
+        const start = tokens.findIndex((token) => token.includes('{'));
+        const end = tokens.findIndex((token) => token.includes('}'));
+        const element_identifier = tokens[start];
+        const element_identifier_split = element_identifier.slice(1).split(':');
+        let properties = '';
+        for (let i = start + 1; i <= end; i++) {
+            if (i < end) {
+                properties += tokens[i] + ' ';
+            } else {
+                properties += tokens[i];
+            }
+        }
+        properties = properties.substring(0, properties.length-1);
+
+        return `//${element_identifier_split[0]}[@${element_identifier_split[1]}='${properties}']`;
+    }
+
+    function getParametersWithElementIdentifier(tokens) {
+        const start = tokens.findIndex((token) => token.includes('}'));
+        let parameters = '';
+        for (let i = start + 1; i < tokens.length; i++) {
+            if (i < tokens.length - 1) {
+                parameters += tokens[i] + ' ';
+            } else {
+                parameters += tokens[i];
+            }
+        }
+
+        return parameters;
+    }
+
+    function getWaitLength(tokens) {
+        return tokens[1];
+    }
+
     // EXECUTION
-    // inputTextOnElement("//textarea[@aria-label='Search']", "Hello world!");
-    // await waitFor(1500);
-    // clickElement("//input[@value='Google Search']");
-    parseTestCase();
+    const state = await parseTestCase();
+    const testcase = state.testcase;
+
+    for (const line of byLine(testcase)) {
+        const tokens = bySpaces(line);
+
+        switch (tokens[0]) {
+            case 'click':
+                const click_expr = getExpressionWithElementIdentifier(tokens);
+                console.log('Click on', click_expr);
+                clickElement(click_expr);
+                break;
+            case 'enter':
+                const enter_expr = getExpressionWithElementIdentifier(tokens);
+                console.log('Enter on', enter_expr);
+                pressEnterOnElement(enter_expr);
+                break;
+            case 'input':
+                const input_expr = getExpressionWithElementIdentifier(tokens);
+                const input_params = getParametersWithElementIdentifier(tokens);
+                console.log('Input on', input_expr, 'with', input_params);
+                inputTextOnElement(input_expr, input_params);
+                break;
+            case 'wait':
+                const wait_length = getWaitLength(tokens);
+                console.log('Wait for', wait_length, 'ms.');
+                await waitFor(parseInt(wait_length));
+                break;
+        }
+    }
 }
 
 // On Extension click
